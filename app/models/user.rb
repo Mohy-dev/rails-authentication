aedfaf4ebdd9e61e03acc54b24fcc6882a2a5233
mohy-dev <mohy.dev@gmail.com>
2022-08-02 01:20:39 +0200

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   CONFIRMATION_TOKEN_EXPIRATION = 10.minutes
 
+  MAILER_FROM_EMAIL = "no-reply@example.com"
+
+
   has_secure_password
 
-  before_save :downcase_email
+  before_save :down_case_email
 
   validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
 
@@ -13,19 +16,33 @@ class User < ApplicationRecord
   end
 
   def confirmed?
-    confirm_at.present?
+    confirmed_at.present?
   end
 
   def generate_confirmation_token
     signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
   end
 
-  def unconfirmed!
+  def unconfirmed?
     !confirmed?
   end
 
   def down_case_email
     self.email = email.downcase
+  end
+
+  def send_confirmation_email!
+    confirmation_token = generate_confirmation_token
+    UserMailer.confirmation(self, confirmation_token).deliver_now
+  end
+
+  def generate_password_reset_token
+    signed_id expires_in: PASSWORD_RESET_TOKEN_EXPIRATION, purpose: :reset_password
+  end
+
+  def send_password_reset_email!
+    password_reset_token = generate_password_reset_token
+    UserMailer.password_reset(self, password_reset_token).deliver_now
   end
 
 end
