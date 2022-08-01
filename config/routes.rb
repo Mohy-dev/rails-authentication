6d0ce47f50c39bb2c54b6255e90e38f6b93343e1
mohy-dev <mohy.dev@gmail.com>
2022-08-01 18:54:55 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+
+  resources :confirmations, only: [:create, :edit, :new], param: confirmation_token
 end
