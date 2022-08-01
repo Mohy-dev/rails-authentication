9444fb32d18ca56191389bdf3f3efc1a7c35e796
mohy-dev <mohy.dev@gmail.com>
2022-08-01 18:56:20 +0200

    Add confrimation and user controller

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,28 @@
+class ConfirmationsController < ApplicationController
+
+  def create
+    @user = User.find_by(email: params[:user][:email].downcase)
+
+    if @user.persent? && @user.unconfirmed?
+      redirect_to  root_path, notice: "Check your email for confirmation instructions"
+    else
+      redirect_to  root_path, notic: "We couldn't find a user with that email address"
+    end
+  end
+
+  def edit
+    @user = User.find_signed(params[:confirmation_token], purpose: :confirm_email)
+
+    if @user.persent?
+      @user.confirm!
+      redirect_to root_path, notice: "Your email address has been successfully confirmed"
+    else
+      redirect_to root_path, notice: "Invalid token"
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+end
