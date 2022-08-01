de47a35a2956569d0a4df10ff3371f8c2b7ee35c
mohy-dev <mohy.dev@gmail.com>
2022-08-02 01:21:18 +0200

    views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   post "sign_up", to: "users#create"
   get "sign_up", to: "users#new"
 
-  resources :confirmations, only: [:create, :edit, :new], param: confirmation_token
+  post "login", to:"sessions#create"
+  delete "logout", to:"sessions#destroy"
+  get "login", to:"sessions#new"
+
+  resources :passwords, only: [:create, :edit, :new, :update], param: :password_reset_token
+
+  resources :confirmations, only: [:create, :edit, :new], param: :confirmation_token
 end
